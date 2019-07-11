2525dcc2743c804eea7e922e4b9ccca4f6a6122e
bimuyu <bimuyu@helijia.com>
2019-07-11 18:31:07 +0800

    add email

---
+++ blog/emails.py
@@ -0,0 +1,47 @@
+from flask_mail import Message
+from flask import url_for, current_app
+
+from threading import Thread
+
+from blog.extensions import mail
+
+
+def _send_async_mail(app, message):
+    with app.app_context():
+        mail.send(message)
+
+
+def send_mail(subject, to, html):
+    """因为程序实例是通过工厂函数构建的，所以实例化Thread类时，需要使用current_app
+    作为参数列表中的app值。在新建线程时需要真正的程序对象来创建上下文，所以不能直接传
+    入current_app而是需要传入current_app的_get_current_object()方法来获取到被代
+    理的程序实例
+
+    :param subject:
+    :param to:
+    :param html:
+    :return:
+    """
+    app = current_app._get_current_object()
+    message = Message(subject, recipients=[to], html=html)
+    thr = Thread(target=_send_async_mail, args=[app, message])
+    thr.start()
+    return thr
+
+
+def send_new_comment_email(post):
+    post_url = url_for('blog.show_post', post_id=post.id, _external=True) + '#comments'
+    send_mail(subject='New comment', to=current_app.config['BLUELOG_EMAIL'],
+              html='<p>New comment in post <i>%s</i>, click the link below to check:</p>'
+                   '<p><a href="%s">%s</a></P>'
+                   '<p><small style="color: #868e96">Do not reply this email.</small></p>'
+                   % (post.title, post_url, post_url))
+
+
+def send_new_reply_email(comment):
+    post_url = url_for('blog.show_post', post_id=comment.post_id, _external=True) + '#comments'
+    send_mail(subject='New reply', to=comment.email,
+              html='<p>New reply for the comment you left in post <i>%s</i>, click the link below to check: </p>'
+                   '<p><a href="%s">%s</a></p>'
+                   '<p><small style="color: #868e96">Do not reply this email.</small></p>'
+                   % (comment.post.title, post_url, post_url))
